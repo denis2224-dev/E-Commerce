242d6202ceb5ead1a1ea52808259ae85d678e171
Denis <denymoroz2006@gmail.com>
2026-05-17 22:02:17 +0300

    Style ecommerce header

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  brandName = 'E-Commerce';
+
+  navItems = [
+    { label: 'Products', path: '/products' },
+    { label: 'Categories', path: '/categories' },
+    { label: 'Deals', path: '/deals' },
+    { label: 'Cart', path: '/cart' },
+    { label: 'Account', path: '/account' },
+  ];
+}
