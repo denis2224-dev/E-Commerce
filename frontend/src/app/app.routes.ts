88f62f8efa7c3526376c3a30d90a1a3e4b8a3743
Denis <denymoroz2006@gmail.com>
2026-05-18 18:42:33 +0300

    Add create account page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { SignIn } from './pages/sign-in/sign-in';
+import { CreateAccount } from './pages/create-account/create-account';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'sign-in', component: SignIn },
+  { path: 'create-account', component: CreateAccount },
   { path: '**', redirectTo: '' },
 ];
